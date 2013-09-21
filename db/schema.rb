6d507948f59a9c88641d9b6e6ab7cf04c2c8d51b
Tyler Morgan <tylermorgan86@gmail.com>
2013-09-21 13:31:33 -0700

    working on geo location

--- db/schema.rb
+++ db/schema.rb
@@ -13,17 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20130921202247) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "kid_locations", force: true do |t|
-    t.string   "address"
-    t.float    "latitude"
-    t.float    "longitude"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "kids", force: true do |t|
     t.string   "case_number"
     t.string   "org_prefix"
