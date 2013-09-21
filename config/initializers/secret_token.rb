655c93892f38e3de3cbb7463574e57a9064fc136
theverything <j3ffhorn@gmail.com>
2013-09-21 09:12:48 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Four04kids::Application.config.secret_key_base = 'cc1255af910705f42ede76dced95d5a33c93ca3e8556900b084745719e4fe847b1ea147e3d7a9529facd4bb6fa4afb929ebf1fbb78dfbe1f07cdc283e0e0b84f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Four04kids::Application.config.session_store :cookie_store, key: '_four04kids_session'
