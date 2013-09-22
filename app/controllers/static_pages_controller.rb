2d44865d425402302a7f8b708bcbc50db27fc7c6
theverything <j3ffhorn@gmail.com>
2013-09-22 13:45:25 -0700

    refactor page layouts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,21 +1,28 @@
 class StaticPagesController < ApplicationController
-  before_filter :fetch_kids
 
   def home
+    fetch_kids
   end
 
   def about
+    fetch_kids(6)
   end
 
   def stories
+    fetch_kids(6)
   end
 
   def donate
+    fetch_kids(6)
+  end
+
+  def docs
+    fetch_kids(6)
   end
 
   private
 
-  def fetch_kids
+  def fetch_kids(number_of_entries = 30)
     if params[:location].blank?
       @location_query = [location.latitude, location.longitude]
       @city = request.location.city
@@ -24,6 +31,6 @@ class StaticPagesController < ApplicationController
       @city = params[:city]
     end
     @kids = Kid.limit(50).near(@location_query, 500)
-    @kids = @kids.to_a.slice(0,30).shuffle
+    @kids = @kids.to_a.slice(0,number_of_entries).shuffle
   end
 end
