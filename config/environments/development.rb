33a7de717a561f29501e028eca6c9e11ae67dc91
Hank Stoever <hstove@gmail.com>
2013-09-21 22:08:52 -0700

    use `image-url` in scss

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,6 @@ Four04kids::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.serve_static_assets = true
 end
