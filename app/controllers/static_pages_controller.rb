7a51485a98deeea20924e0b8a64249dbeb3c5374
Tyler Morgan <tylermorgan86@gmail.com>
2013-09-21 19:46:29 -0700

    working on styling

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,7 +9,12 @@ class StaticPagesController < ApplicationController
   end
 
   def about
-
+    if request.ip == "127.0.0.1"
+      req_ip = "173.160.181.220"
+    else
+      req_ip = request.ip
+    end
+    @kids = Kid.near(req_ip, 500)
   end
 
   def stories
