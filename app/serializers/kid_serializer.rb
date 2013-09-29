8f70ce7e340b86df6f084e711957c3c56532baf4
theverything <j3ffhorn@gmail.com>
2013-09-29 16:46:10 -0700

    add new attributes to kids json output

--- app/serializers/kid_serializer.rb
+++ app/serializers/kid_serializer.rb
@@ -4,6 +4,6 @@ class KidSerializer < ActiveModel::Serializer
 
   attributes :id, :missing_city, :missing_state, :missing_county, :missing_country, :missing_date,
     :age, :thumbnail_url, :image_url, :aged_photo_url, :case_number, :org_prefix, :first_name,
-    :last_name, :middle_name, :age, :full_name, :missing_url
+    :last_name, :middle_name, :age, :full_name, :missing_url, :weight, :height, :eye_color, :hair_color, :race, :sex, :circumstance, :has_aged_photo
 
 end
